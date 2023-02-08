5a16a72dbdc0b4a85e04df3397fccb143858de6f
Pernille Kopperud <pernille@griegconnect.com>
2023-02-08 16:22:15 +0100

    updating fine-tuning pipeline

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -7,32 +7,59 @@ from torchvision import transforms, datasets
 from matplotlib import pyplot as plt
 import numpy as np
 
-def load_dataset(img_path : str):
+def load_dataset(img_path : str, batch_size : int, transforms):
     
-    tsfm = transforms.Compose([
-        transforms.ToTensor()
-])
-    data = datasets.ImageFolder(img_path, tsfm)
-    dataloader = DataLoader(data, batch_size=20)
+    data = datasets.ImageFolder(img_path, transforms)
+
+    length = [round(len(data)*0.8), round(len(data)*0.2)]
+    train_data, val_data = torch.utils.data.random_split(data, length)
+
+    training_data_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=2)
+    validation_data_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True, num_workers=2)
+    '''
     for batch in dataloader:
         inputs, targets = batch
-        for img in inputs:
-            image  = img.cpu().numpy()
+    '''
+
+    return training_data_loader, validation_data_loader
+
+def test_image_loader(training_data_loader, validation_data_loader):
+    for train_batch, val_batch in zip(training_data_loader, validation_data_loader):
+        train_inputs, train_targets = train_batch
+        val_inputs, val_targets = val_batch
+        counter = 0
+        for train_img, val_img in zip(train_inputs, val_inputs):
+            train_image  = train_img.cpu().numpy()
+            val_image  = val_img.cpu().numpy()
             # transpose image to fit plt input
-            image = image.T
+            train_image = train_image.T
+            val_image = val_image.T
+
             # normalise image
-            data_min = np.min(image, axis=(1,2), keepdims=True)
-            data_max = np.max(image, axis=(1,2), keepdims=True)
-            scaled_data = (image - data_min) / (data_max - data_min)
+            train_data_min = np.min(train_image, axis=(1,2), keepdims=True)
+            train_data_max = np.max(train_image, axis=(1,2), keepdims=True)
+            train_scaled_data = (train_image - train_data_min) / (train_data_max - train_data_min)
+
+             # normalise image
+            val_data_min = np.min(train_image, axis=(1,2), keepdims=True)
+            val_data_max = np.max(train_image, axis=(1,2), keepdims=True)
+            val_scaled_data = (val_image - val_data_min) / (val_data_max - val_data_min)
+
             # show image
-            plt.imshow(scaled_data)
-            plt.show()
+            plt.imshow(train_scaled_data)
+            plt.savefig("train_" + str(counter)+ ".png")
+            plt.imshow(val_scaled_data)
+            plt.savefig("val" + str(counter)+ ".png")
+            counter = counter + 1
 
-    return dataloader
-
-def data_preprocessing():
+def data_preprocessing_arcface():
     
     return
 
 
-load_dataset("/mnt/c/Users/PernilleKopperud/Documents/InfoSec/MasterThesis/master_thesis/MSc_FR_Bias/src/datasets/lfw")
+
+tsfm = transforms.Compose([
+        transforms.ToTensor()
+])
+
+load_dataset("/mnt/c/Users/PernilleKopperud/Documents/InfoSec/MasterThesis/master_thesis/MSc_FR_Bias/src/datasets/lfw", 20, tsfm)
--- src/optimization/fine_tuning_pipeline.py
+++ src/optimization/fine_tuning_pipeline.py
@@ -39,6 +39,71 @@ def unfreeze_model_layers(frozenParams: list, frozenLayers, model : m):
 
     return model
 
+
+def train_one_epoch(training_data_loader, optimizer, model, loss_function):
+    running_loss = 0.
+    last_loss = 0.
+
+    for i, data in enumerate(training_data_loader):
+        inputs, labels = data
+
+        # Zero your gradients for every batch
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_function(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            running_loss = 0.
+
+    return last_loss
+
+def train_model(number_of_epochs : int, model, learning_rate, momentum, training_data_loader, validation_data_loader):
+    epoch_number = 0
+    best_vloss = 1_000_000.
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    for epoch in range(number_of_epochs):
+        print('EPOCH {}:'.format(epoch_number + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(training_data_loader, optimizer, loss_fn)
+
+        # We don't need gradients on to do reporting
+        model.train(False)
+
+        running_vloss = 0.0
+        for i, vdata in enumerate(validation_data_loader):
+            vinputs, vlabels = vdata
+            voutputs = model(vinputs)
+            vloss = loss_fn(voutputs, vlabels)
+            running_vloss += vloss
+
+        avg_vloss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+        # Track best performance, and save the model's state
+        if avg_vloss < best_vloss:
+            best_vloss = avg_vloss
+            model_path = 'model_{}'.format(epoch_number)
+            torch.save(model.state_dict(), model_path)
+
+        epoch_number += 1
+
+
 '''
 Method fine-tunes a pre-trained model with new data
 Input: 
@@ -51,12 +116,16 @@ def fine_tuning_pipeline(filename : str, device : torch.device, frozenParams: li
     model = load_pretrained_model(filename, device)
     model = unfreeze_model_layers(frozenParams, frozenLayers)
 
-    # Load dataset
-    data, labels = load_dataset(path)
-
-    # Preprocess data
-    
+    # Load training and validation dataset
+    training_data_loader, validation_data_loader = load_dataset(path)
 
     # Train the unfrozen layers
+    train_model(10, model, 0.001, 0.09, training_data_loader, validation_data_loader)
 
-    return
+
+frozenParams = ['conv1.weight', 'bn1.weight', 'bn1.bias',  'prelu.weight']
+frozenLayers = ['layer1', 'layer2']
+module : torch.nn.Module = iresnet50()
+input_images_path = "datasets/cusp_generated/"
+
+fine_tuning_pipeline("models/backbone.pth", torch.device('cuda', 0), frozenParams, frozenLayers, module, input_images_path)
