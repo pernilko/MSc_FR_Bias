b4db20a3e02b15eb679133cac43b4f0b7b0d5eb3
Pernille Kopperud <pernille@griegconnect.com>
2023-02-09 22:34:37 +0100

    eval perf

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -9,11 +9,11 @@ def evaluate_performance(model, test_data_loader):
             print("labels: ", labels)
             outputs = model(images)
             print("outputs:", outputs)
-            _, predictions = torch.max(outputs.data, 1)
+            _, predictions = torch.max(outputs, 1)
             print("Predict: ", predictions)
 
-            true_positives, false_positives, true_negatives, false_negatives = performance_measure(labels, predictions)
-            print("TP: ", true_positives, "FP: ", false_positives, "TN: ", true_negatives, "FN: ", false_negatives)
+            class_id, true_positives, false_positives, true_negatives, false_negatives = performance_measure(labels, predictions)
+            print("class_id: ", class_id, "TP: ", true_positives, "FP: ", false_positives, "TN: ", true_negatives, "FN: ", false_negatives)
 
             far, frr = calculate_far_frr(false_positives, false_negatives, true_negatives, true_positives)
             print ("FAR: ", far, "FRR: ", frr)
@@ -30,15 +30,21 @@ def performance_measure(actual_labels, predicted_labels):
     false_positives = 0
     true_negatives = 0
     false_negatives = 0
+    class_id = set(actual_labels).union(set(predicted_labels))
 
-    for i in range(len(predicted_labels)): 
-        if actual_labels[i]==predicted_labels[i]==1:
-           true_positives += 1
-        if predicted_labels[i]==1 and actual_labels[i]!=predicted_labels[i]:
-           false_positives += 1
-        if actual_labels[i]==predicted_labels[i]==0:
-           true_negatives += 1
-        if predicted_labels[i]==0 and actual_labels[i]!=predicted_labels[i]:
-           false_negatives += 1
+    for index ,_id in enumerate(class_id):
+        true_positives.append(0)
+        false_positives.append(0)
+        true_negatives.append(0)
+        false_negatives.append(0)
+        for i in range(len(predicted_labels)):
+            if actual_labels[i] == predicted_labels[i] == _id:
+                true_positives[index] += 1
+            if predicted_labels[i] == _id and actual_labels[i] != predicted_labels[i]:
+                false_positives[index] += 1
+            if actual_labels[i] == predicted_labels[i] != _id:
+                true_negatives[index] += 1
+            if predicted_labels[i] != _id and actual_labels[i] != predicted_labels[i]:
+                false_negatives[index] += 1
 
-    return(true_positives, false_positives, true_negatives, false_negatives)
+    return(class_id, true_positives, false_positives, true_negatives, false_negatives)
