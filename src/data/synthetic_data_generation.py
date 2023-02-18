51f6954d2a873a3b54dc0ec260632fbd1e00ced4
Pernille Kopperud <pernille@griegconnect.com>
2023-02-18 16:07:58 +0100

    test

--- src/data/synthetic_data_generation.py
+++ src/data/synthetic_data_generation.py
@@ -214,7 +214,7 @@ def run(images_path : str, aging_steps : int):
     FFHQ_LS_KEY: dict(
         gdrive_id="1sWSH3tHgm9DkHrc19hoEMrR-KQgnaFuw",
         side=256, 
-        classes=(0,70)),
+        classes=(1,70)),
     FFHQ_RR_KEY: dict(
         gdrive_id="17BOTEa6z3r6JFVs1KDutDxWEkTWbzaeD",
         side=224, 
