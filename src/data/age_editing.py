1011fa946547fb8c55bdab5f6d832ee0026684ec
Pernille Kopperud <pernille@griegconnect.com>
2023-02-26 17:36:28 +0100

    trying to fix genration in age bins

--- src/data/age_editing.py
+++ src/data/age_editing.py
@@ -64,6 +64,7 @@ Method
 def generate_synthetic_data(G, img, label, global_blur_val=None, mask_blur_val=None, return_msk=False):
     #print(G.attr_map.fc0.init_args[0])
     G.attr_dim = 12
+    print(G.attr_map.fc0.init_args[0])
     ohe_label = torch.nn.functional.one_hot(torch.tensor(label), num_classes=G.attr_map.fc0.init_args[0]).to(img.device)
     c_out, c_out_skip = G.content_enc(img)
     
