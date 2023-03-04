9a963eead18abaff3b4ec9cc54c9fa2ec7268cdd
Pernille Kopperud <pernille@griegconnect.com>
2023-03-04 12:20:28 +0100

    age editing e3gd

--- src/data/age_editing_v2.py
+++ src/data/age_editing_v2.py
@@ -52,8 +52,8 @@ def age_editing_e(device : torch.device, network_pkl, input_images_path : str, t
     for img_tensor in inp_images_tensor:
 
         print("img tensor: ", img_tensor)
-        z = img_tensor.to(device).mode(2)
-        #print(z.ndim)
+        z = img_tensor.to(device)
+        print(z.ndim)
         print(G.z_dim)
         fov_deg = 18.837
         intrinsics = FOV_to_intrinsics(fov_deg, device=device)
