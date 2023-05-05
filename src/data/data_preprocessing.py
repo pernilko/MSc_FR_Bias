3dd8a845acb8d837f91c0fe849562efcb5599092
Pernille Kopperud <pernille@griegconnect.com>
2023-05-05 19:43:47 +0200

    modify plot mixed dataset

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -163,7 +163,7 @@ def sort_filenames(filenames):
                     next = next.strip('abcdef')
                     if int(current) < int(age) and int(next) > int(age):
                         list.insert(list.index(list[e+1]), filenames[i])
-                    elif int(current) > int(age):
+                    elif int(current) > int(age) and int(current) != int(age):
                         list.insert(list.index(list[e]), filenames[i])
     return list
 
